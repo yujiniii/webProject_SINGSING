58b43bfd629444dcc945183333ea742779eeb1c8
서유진 <50348197+yujiniii@users.noreply.github.com>
2022-06-08 02:29:31 +0900

    Create view files

--- routes/repair.js
+++ routes/repair.js
@@ -2,10 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get("/",(req,res)=>{
-    res.sendFile('../views/chat.html');
+router.get('/', function(req,res){
+    res.render('404');
 });
-
 
 
 module.exports = router;
