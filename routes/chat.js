4c44cc7d501fe183d2970f55edfa5f797999d831
서유진 <50348197+yujiniii@users.noreply.github.com>
2022-06-11 21:27:03 +0900

    [feat] chatting

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const { render } = require('express/lib/response');
 const router = express.Router();
 
 
 router.get("/",(req,res)=>{
-    res.sendFile('../views/chat.html');
+    //res.sendFile('../views/chat.html');
+    res.render('chat')
 });
 
 
