e1fe60b595bc13211f1658afafd49ec2753fcbfe
whdbsdk1115 <whdbsdk1115@naver.com>
2022-06-12 19:35:40 +0900

    repair
    예약/수정 - 날짜-시간으로 분리
    	현재날짜 이후 달력 선택 가능
    	예약 시간은 10-18까지 가능(30분 단위), 이외에 시간 선택도 가능하지만 예약 시 걸림
    	날짜/시간/기사가 같은 내역이 DB에 있으면 추가 못함
    
    기사별 예약 내역 - 해당 기사 이름 추가
    
    css 아주 조금 추가(버튼 같은 거 ..)

--- routes/repair.js
+++ routes/repair.js
@@ -13,7 +13,7 @@ const ObjectId = mongodb.ObjectId;
 router.get('/', async (req,res,next)=>{
     // DB에서 'repair' collection을 찾아 보여줌
     const repairs = await db.getDb().collection('repair')
-    .find({}, {date:1, repairman:1,
+    .find({}, {date:1, time:1, repairman:1,
         model:1, bought:1, reason:1, name:1}).toArray();
 
     // repairs-list.ejs
@@ -22,31 +22,22 @@ router.get('/', async (req,res,next)=>{
 
 router.post('/select-repairman', async(req, res,next)=>{
     console.log(req.body.repairman);
+    const man = req.body.repairman;
     const select_repairman = await db.getDb().collection('repair')
     .find({repairman: req.body.repairman}, 
-        {date:1, repairman:1, model:1, bought:1, reason:1, name:1, phone:1,adress:1} )
-    .toArray();
+        {date:1, time:1, repairman:1, model:1, bought:1, reason:1, 
+            name:1, phone:1,adress:1} ).toArray();
+
     console.log(select_repairman);
-    res.render('repairman-list', {select_repairman:select_repairman});
+    res.render('repairman-list', {select_repairman:select_repairman, man:man});
 }); // ejs에서 선택한 수리기사 받기 
-
-
-/*
-router.get('/select-repairman', async(req, res, next) => {
-    // DB에서 'repair' collection을 찾아 보여줌
-    const select_repairman = await db.getDb().collection('repair')
-    .find({repairmain: req.body.repairman}, {date:1, repairman:1,
-        model:1, bought:1, reason:1, name:1}).toArray();
-
-    // repairs-list.ejs
-    res.render('repairman-list', {select_repairman:select_repairman});
-});*/
 
 
 router.post('/', async (req,res,next)=>{
     console.log(req.body);
     const newRepair = {
         date : req.body.date,
+        time: req.body.time,
         repairman : req.body.repairman,
         
         model : req.body.model,
@@ -56,34 +47,51 @@ router.post('/', async (req,res,next)=>{
         name : req.body.name,
         phone : req.body.phone,
         address : req.body.address,
-    }    
-    // DB에 예약내역 추가
-    const result = await db.getDb().collection('repair').insertOne(newRepair);
-    console.log(result);
-    res.redirect('/repair');
+    }   
+    const dd = req.body.date;
+    const tt = req.body.time;
+    const rr = req.body.repairman;
+
+    // 예약하려는 내역과 같은 날짜, 시간, 기사님이 하나라도 있다면 예약 불가
+    const inDt = await db.getDb().collection('repair').findOne({date:dd, time:tt, repairman:rr},{date:1, time:1});
+    if(inDt.date==dd) { // 날짜가 같을 때
+        if(inDt.time==tt) { // 시간이 같을 때
+            if(inDt.repairman==rr) { // 기사가 같을 때
+                res.render('create-error');
+            } else { // 날짜, 시간, 기사가 모두 다를 때
+                // DB에 예약내역 추가
+                const result = await db.getDb().collection('repair').insertOne(newRepair);
+                console.log(result);
+                res.redirect('/repair');
+            } 
+        }
+    } 
+    console.log(888);
+    
 }); // 설치 및 수리 예약하기(CREATE)
+
 
 router.get('/new', async (req,res,next)=>{
     // create-repair.ejs
     res.render('create-repair');
 }); // 설치 및 수리 화면
 
-
 router.get('/:id/phone-auth', async (req,res,next)=>{
     res.render('phone-auth', {id : req.params.id})
 }); // 수정 시 전화번호 검증화면
 
-
 router.post('/:id/phone-auth', async (req,res,next)=>{
+    // id에 맞는 내역 검색
     const repair = await db.getDb().collection('repair')
     .findOne({_id: new ObjectId(req.params.id)},
-    {date:1, repairman:1, model:1, bought:1, reason:1, 
+    {date:1, time:1, repairman:1, model:1, bought:1, reason:1, 
         name:1, phone:1, address:1});    
 
+    // 비밀번호가 일치했을 때
     if(repair.phone === req.body.phone){
         res.render('modify-repair',{repair:repair})
     }
-    // 비밀번호가 틀리면 phone-error.ejs
+    // 비밀번호가 다를 때
     res.render('phone-error') 
 }); 
 
@@ -92,10 +100,11 @@ router.get('/:id/update', async (req,res,next)=>{
 }); // 예약내역 수정하기(UPDATE)
 
 router.post('/:id/update', async (req,res,next)=>{
-    await db.getDb().collection('repair')
+    const result = await db.getDb().collection('repair')
     .updateOne({_id: new ObjectId(req.params.id)},
     { $set: {
         date : req.body.date,
+        time: req.body.time,
         repairman : req.body.repairman,
     
         model : req.body.model,
@@ -106,14 +115,15 @@ router.post('/:id/update', async (req,res,next)=>{
         phone : req.body.phone,
         address : req.body.address
     }});
-    res.redirect('/repair')
+    console.log(result);
+    res.redirect('/repair');
 }); // 예약내역 수정하기(UPDATE)
 
 
 router.get('/:id/delete', async (req,res,next)=>{
-    await db.getDb().collection('repair')
+    const result = await db.getDb().collection('repair')
     .deleteOne({_id: new ObjectId(req.params.id)});
-
+    console.log(result);
     res.redirect('/repair')
 }); // 예약내역 삭제하기(DELETE)
 
